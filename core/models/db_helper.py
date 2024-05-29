d4c6360b35c18f470bf009fec0c2716aeaeffad4
Pasha Ryaguzov <pashajobber@gmail.com>
2024-05-29 19:07:58 +0500

    create db helper (session, generator)

---
+++ core/models/db_helper.py
@@ -0,0 +1,60 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core.config import settings
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool,
+        echo_pool: bool,
+        pool_size: int,
+        max_overflow: int,
+    ) -> None:
+        """
+        Создание асинхронного движка и фабрики сессий
+        """
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self) -> None:
+        """
+        Закрываем движок -> закрытие соединение,
+        которое используется этим движком
+        """
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        """
+        Даем возможность получить сессию в любом месте
+        """
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow,
+)
