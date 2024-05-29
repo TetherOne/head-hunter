b9f5ab67baac332665b374e5bba2eca6adb9bae5
Pasha Ryaguzov <pashajobber@gmail.com>
2024-05-29 19:48:13 +0500

    ad black

--- alembic/versions/2024_05_29_1941-4ed76849369d_create_vacancy_model.py
+++ alembic/versions/2024_05_29_1941-4ed76849369d_create_vacancy_model.py
@@ -5,32 +5,44 @@ Revises: 0476b04db854
 Create Date: 2024-05-29 19:41:46.855043
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '4ed76849369d'
-down_revision: Union[str, None] = '0476b04db854'
+revision: str = "4ed76849369d"
+down_revision: Union[str, None] = "0476b04db854"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('vacancys',
-    sa.Column('job_tittle', sa.String(), nullable=False),
-    sa.Column('description', sa.String(), nullable=False),
-    sa.Column('address', sa.String(), nullable=True),
-    sa.Column('phone', sa.String(), nullable=True),
-    sa.Column('email', sa.String(), nullable=True),
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=False),
-    sa.Column('updated_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=False),
-    sa.PrimaryKeyConstraint('id'),
+    op.create_table(
+        "vacancys",
+        sa.Column("job_tittle", sa.String(), nullable=False),
+        sa.Column("description", sa.String(), nullable=False),
+        sa.Column("address", sa.String(), nullable=True),
+        sa.Column("phone", sa.String(), nullable=True),
+        sa.Column("email", sa.String(), nullable=True),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("id"),
     )
 
 
 def downgrade() -> None:
-    op.drop_table('vacancys')
+    op.drop_table("vacancys")
