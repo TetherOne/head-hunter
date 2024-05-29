0de82e05375f1b736f2e98d0a2bbc042efea6e8d
Pasha Ryaguzov <pashajobber@gmail.com>
2024-05-29 16:10:19 +0500

    create base model

---
+++ core/models/base.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+
+from utils.case_converter import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
