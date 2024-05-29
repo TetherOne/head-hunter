167453dab7aebfe94f56696cc9c2d87025415fec
Pasha Ryaguzov <pashajobber@gmail.com>
2024-05-29 23:58:12 +0500

    create schemas

---
+++ api/resumes/schemas.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class ResumeBase(BaseModel):
+    job_tittle: str
+    description: str
+    salary: int
+
+
+class Resume(ResumeBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
