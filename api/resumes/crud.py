53811f21905876ac8be3d34bb4ba0be2133fed41
Pasha Ryaguzov <pashajobber@gmail.com>
2024-05-31 09:36:42 +0500

    ad create_resume

--- api/resumes/crud.py
+++ api/resumes/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Sequence
 
+from api.resumes.schemas import ResumeCreate
 from core.models import Resume
 
 
@@ -18,3 +19,14 @@ async def get_resume(
     session: AsyncSession,
 ) -> Resume | None:
     return await session.get(Resume, resume_id)
+
+
+async def create_resume(
+    resume_create: ResumeCreate,
+    session: AsyncSession,
+) -> Resume:
+    resume = Resume(**resume_create.model_dump())
+    session.add(resume)
+    await session.commit()
+    await session.refresh(resume)
+    return resume
