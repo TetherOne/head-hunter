7b16d7f71490b8567c54b4acee95566800a461d4
Pasha Ryaguzov <pashajobber@gmail.com>
2024-06-01 00:35:47 +0500

    ad aiobotocore

---
+++ core/s3_db/connection.py
@@ -0,0 +1,28 @@
+from contextlib import asynccontextmanager
+
+from aiobotocore.session import get_session
+
+
+class S3Client:
+    def __init__(
+        self,
+        access_key: str,
+        secret_key: str,
+        endpoint_url: str,
+        bucket_name: str,
+    ):
+        self.config = {
+            "aws_access_key_id": access_key,
+            "aws_secret_access_key": secret_key,
+            "endpoint_url": endpoint_url,
+        }
+        self.bucket_name = bucket_name
+        self.session = get_session()
+
+    @asynccontextmanager
+    async def get_client(self):
+        async with self.session.create_client(
+                "s3",
+                **self.config,
+        ) as client:
+            yield client
