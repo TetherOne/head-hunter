025023bad23f705b1b375c7c0b6e632d0c037102
Pasha Ryaguzov <pashajobber@gmail.com>
2024-05-31 09:37:11 +0500

    ad resume_create and get_resume apis

--- api/resumes/views.py
+++ api/resumes/views.py
@@ -4,7 +4,8 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.resumes import crud
-from api.resumes.schemas import Resume
+from api.resumes.dependencies import resume_by_id
+from api.resumes.schemas import Resume, ResumeCreate
 from core.models import db_helper
 
 
@@ -25,5 +26,34 @@ async def get_resumes(
     ],
 ):
     return await crud.get_resumes(
+        session=session,
+    )
+
+
+@router.get(
+    "/{resume_id}",
+    response_model=Resume,
+    status_code=status.HTTP_200_OK,
+)
+async def get_resume(
+    resume: Resume = Depends(resume_by_id),
+):
+    return resume
+
+
+@router.post(
+    "/create/",
+    response_model=ResumeCreate,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_resume(
+    resume_create: ResumeCreate,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+):
+    return await crud.create_resume(
+        resume_create=resume_create,
         session=session,
     )
