6b547c0e6cecefe18ee8595a5c77933f2bf14406
Pasha Ryaguzov <pashajobber@gmail.com>
2024-06-01 00:35:29 +0500

    ad image to resume

--- core/models/resume.py
+++ core/models/resume.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 from sqlalchemy.orm import Mapped
 
 from core.models.base import Base
 
 
 class Resume(Base):
+    image: Mapped[Optional[str]]
     job_tittle: Mapped[str]
     description: Mapped[str]
     salary: Mapped[int]
