753ae655b49d2f01bec177e610366d404da7729b
Pasha Ryaguzov <pashajobber@gmail.com>
2024-06-04 20:21:27 +0500

    ad some for auth

--- api/auth/utils.py
+++ api/auth/utils.py
@@ -1,28 +1,44 @@
-import jwt
-from core.config import settings
-
-
-def encode_jwt(
-    payload: dict,
-    private_key: str = settings.auth_jwt.private_key_path.read_text(),
-    algorithm: str = settings.auth_jwt.algorithm,
-):
-    encoded = jwt.encode(
-        payload,
-        private_key,
-        algorithm=algorithm,
-    )
-    return encoded
-
-
-def decode_jwt(
-    token: str | bytes,
-    public_key: str = settings.auth_jwt.public_key_path.read_text(),
-    algorithm: str = settings.auth_jwt.algorithm,
-):
-    decoded = jwt.decode(
-        token,
-        public_key,
-        algorithms=[algorithm],
-    )
-    return decoded
+# import bcrypt
+# import jwt
+# from core.config import settings
+#
+#
+# def encode_jwt(
+#     payload: dict,
+#     private_key: str = settings.auth_jwt.private_key_path.read_text(),
+#     algorithm: str = settings.auth_jwt.algorithm,
+# ):
+#     to_encode = payload.copy()
+#     expire = 1
+#     to_encode.update(
+#         exp=expire
+#     )
+#     encoded = jwt.encode(
+#         to_encode,
+#         private_key,
+#         algorithm=algorithm,
+#     )
+#     return encoded
+#
+#
+# def decode_jwt(
+#     token: str | bytes,
+#     public_key: str = settings.auth_jwt.public_key_path.read_text(),
+#     algorithm: str = settings.auth_jwt.algorithm,
+# ):
+#     decoded = jwt.decode(
+#         token,
+#         public_key,
+#         algorithms=[algorithm],
+#     )
+#     return decoded
+#
+#
+# def hash_password(password: str) -> bytes:
+#     salt = bcrypt.gensalt()
+#     pwd_bytes: bytes = password.encode()
+#     return bcrypt.hashpw(pwd_bytes, salt)
+#
+#
+# def validate_password(password: str, hashed_password: bytes) -> bool:
+#     return bcrypt.checkpw(password.encode(), hashed_password)
--- api/auth/views.py
+++ api/auth/views.py
@@ -1,4 +1,34 @@
-from fastapi import APIRouter
-
-
-router = APIRouter()
+# from fastapi import APIRouter, Form, HTTPException, Depends
+# from starlette import status
+#
+# from api.auth import utils
+# from api.auth.schemas import UserSchema
+# from core.models import db_helper, User
+#
+# router = APIRouter()
+#
+#
+#
+# def validate_auth_user(
+#     username: str = Form(),
+#     password: str = Form(),
+#     email: str = Form(),
+# ):
+#
+#
+#
+#
+# @router.post("/login")
+# def auth_user(
+#     user: UserSchema = Depends(validate_auth_user),
+# ):
+#     jwt_payload = {
+#         "sub": user.id,
+#         "username": user.username,
+#         "email": user.email,
+#     }
+#     token = utils.encode_jwt(jwt_payload)
+#     return {
+#         "access_token": token,
+#         "token_type": "bearer",
+#     }
