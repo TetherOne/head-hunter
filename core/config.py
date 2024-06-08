17f8bad3281ac1e4b36b6ec4e9bdceae14422171
Pasha Ryaguzov <pashajobber@gmail.com>
2024-06-08 10:29:51 +0500

    update docker-compose

--- core/config.py
+++ core/config.py
@@ -43,7 +43,7 @@ class AuthJWT(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="envs/.envs",
+        env_file="envs/.env",
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="FAST_API_HH__",
