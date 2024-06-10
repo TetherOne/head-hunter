6fcfb9b37362bb2f7ae6ec6902f33ec76c490031
Pasha Ryaguzov <pashajobber@gmail.com>
2024-06-10 20:59:45 +0500

    delete login view

--- api/users/views.py
+++ api/users/views.py
@@ -1,12 +1,10 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.users import crud
-from api.users.crud import get_user_by_email
-from api.users.schemas import UserRegister, UserSchema, UserLogin
-from api.users.utils import generate_token, validate_password
+from api.users.schemas import UserRegister, UserSchema
 from core.models import db_helper
 
 router = APIRouter(tags=["Users"])
@@ -28,39 +26,3 @@ async def register_user(
         user_register=user_register,
         session=session,
     )
-
-
-@router.post(
-    "/login/",
-)
-async def auth_token(
-    user_login: UserLogin,
-    session: Annotated[
-        AsyncSession,
-        Depends(db_helper.session_getter),
-    ],
-):
-    user = await get_user_by_email(
-        session=session,
-        email=user_login.email,
-    )
-    if user:
-        if not validate_password(
-            user_login.password,
-            user.password,
-        ):
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Invalid credentials",
-            )
-        else:
-            user.token = generate_token()
-            await session.commit()
-            return {
-                "bearer": user.token,
-            }
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Invalid credentials",
-        )
